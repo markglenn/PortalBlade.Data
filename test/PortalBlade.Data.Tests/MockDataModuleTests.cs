baeb3f14c9bb3242fae80ea31594d7e489976841
Mark Glenn <markglenn@gmail.com>
2011-06-13 20:31:15 -0500

    Added MockDataModule tests

--- test/PortalBlade.Data.Tests/MockDataModuleTests.cs
+++ test/PortalBlade.Data.Tests/MockDataModuleTests.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NHibernate;
+using Ninject;
 using NUnit.Framework;
 using PortalBlade.Data.Tests.Models;
 
@@ -14,6 +16,52 @@ namespace PortalBlade.Data.Tests
         public void InitializesInMemorySessions( )
         {
             var dataModule = new MockDataModule( m => m.FluentMappings.Add<PersonMap>( ) );
+
+            using ( var kernel = new StandardKernel( dataModule ) )
+            {
+                using( var session = kernel.Get<ISession>( ) )
+                {
+                    Assert.That( session.Connection.ConnectionString.Contains( ":memory:" ) );
+                }
+            }
+        }
+
+        [Test]
+        public void Session_StaysAliveMultipleConnections( )
+        {
+            var dataModule = new MockDataModule( m => m.FluentMappings.Add<PersonMap>( ) );
+
+            using ( var kernel = new StandardKernel( dataModule ) )
+            {
+                using( var session = kernel.Get<ISession>( ) )
+                {
+                    var repository = session.Repository<Person>( );
+                    repository.Insert( new Person {Name = "Test User"} );
+                }
+
+                using( var session = kernel.Get<ISession>( ) )
+                {
+                    var repository = session.Repository<Person>( );
+                    Assert.AreEqual( "Test User", repository.First( ).Name );
+                }
+            }
+        }
+
+        [Test]
+        public void Repository_CreatesLiveConnection( )
+        {
+            var dataModule = new MockDataModule( m => m.FluentMappings.Add<PersonMap>( ) );
+
+            using ( var kernel = new StandardKernel( dataModule ) )
+            {
+                var repository = kernel.Get<IRepository<Person>>( );
+
+                repository.Insert( new Person {Name = "Testing User"} );
+
+                Assert.AreEqual( 1, repository.Count( ) );
+                Assert.AreEqual( "Testing User", repository.First( ).Name );
+
+            }
         }
     }
 }
